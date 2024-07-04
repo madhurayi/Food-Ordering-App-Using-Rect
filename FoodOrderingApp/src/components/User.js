e831d2508bcf9fbfbc555bf7640205403f4b4146
madhu <rayi.madhupriya@gmail.com>
2024-07-04 07:49:25 +0530

    Added state Variable in Class Component

--- FoodOrderingApp/src/components/User.js
+++ FoodOrderingApp/src/components/User.js
@@ -1,11 +1,15 @@
-const User= ()=>{
-    return (
-        <div className="user-card">
-            <h1>Name: Madhu Priya</h1>
-            <h2>Location: Nellore , Andhra</h2>
-            <h3>contact: rayi.madhupriya@gmail.com</h3>
-        </div>
-    )
-}
+// import { useState } from "react";
 
-export default User;
+// const User= ()=>{
+//     const [count,setCount]=useState(0);
+//     return (
+//         <div className="user-card">
+//             <h1>Name: Madhu Priya</h1>
+//             <h2>Location: Nellore , Andhra</h2>
+//             <h3>count: {count}</h3>
+//             <h3>contact: rayi.madhupriya@gmail.com</h3>
+//         </div>
+//     )
+// }
+
+// export default User;
--- FoodOrderingApp/src/components/UserClass.js
+++ FoodOrderingApp/src/components/UserClass.js
@@ -3,14 +3,21 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            count: 0,
+            count2: 0
+        }
     }
+    
 
     render(){
         const {name,location}=this.props;
+        const {count}= this.state;
         return (
             <div className="user-card">
              <h1>Name: {name}</h1>
             <h2>Location: {location}</h2>
+            <h2>Count: {count}</h2>
             <h3>contact: rayi.madhupriya@gmail.com</h3>
         </div>
         )       
