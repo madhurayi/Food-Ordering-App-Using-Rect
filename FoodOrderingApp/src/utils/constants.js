6f5107536d2484bf67e9c80a5b1cd420be6453a1
madhu <rayi.madhupriya@gmail.com>
2025-05-04 19:11:33 +0530

    Updated constants

---
+++ FoodOrderingApp/src/utils/constants.js
@@ -0,0 +1,6 @@
+
+export const CDN_URL= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+
+export const LOGO_URL="https://img.freepik.com/free-vector/delivery-man-courier-riding-scooter-with-fast-free-application-smartphone_1308-51447.jpg?size=626&ext=jpg&ga=GA1.1.157519005.1709380715&semt=ais_user";
+
+export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.5462927&lng=78.2128113&restaurantId="
